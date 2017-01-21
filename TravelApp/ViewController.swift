50e6e482c258232444de762e3e8666a8ab81460a
Pankaj Rawat <pankajrawat19sept@gmail.com>
2017-01-21 20:50:09 +0530

    Initial Commit

---
+++ TravelApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TravelApp
+//
+//  Created by Pankaj Rawat on 21/01/17.
+//  Copyright © 2017 Pankaj Rawat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
