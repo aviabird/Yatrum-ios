e9ca68f8dfe2bb983788f6242ed0e904c10f8cfc
Pankaj Rawat <pankajrawat19sept@gmail.com>
2017-01-28 18:36:01 +0530

    adding User

---
+++ TravelApp/User.swift
@@ -0,0 +1,14 @@
+//
+//  Trip.swift
+//  TravelApp
+//
+//  Created by rawat on 28/01/17.
+//  Copyright © 2017 Pankaj Rawat. All rights reserved.
+//
+
+import UIKit
+
+class User: NSObject {
+    var name: String?
+    var profileImageURL: String?
+}
