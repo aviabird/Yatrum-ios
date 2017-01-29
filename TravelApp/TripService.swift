254427f4d948af12cce3274d92b9318870bbbfa5
Pankaj Rawat <pankajrawat19sept@gmail.com>
2017-01-29 21:37:02 +0530

    Bug fixes

--- TravelApp/TripService.swift
+++ TravelApp/TripService.swift
@@ -12,71 +12,18 @@ class TripService: NSObject {
 
     static let sharedInstance = TripService()
     static var sharedData = SharedData()
+    let baseUrl = TripService.sharedData.API_URL
     
     func fetchTripsFeed(completion: @escaping ([Trip]) -> () ) {
-        let url = NSURL(string: "\(TripService.sharedData.API_URL)/trips.json")
-        
-        let configuration = URLSessionConfiguration.default
-        
-        let urlRequest = URLRequest(url: url as! URL)
-        
-        let session = URLSession(configuration: configuration)
-        
-        let task = session.dataTask(with: urlRequest) { (data, response, error) -> Void in
-            if (error != nil) {
-                print(error!)
-                return
-            }
-                
-            else {
-                do {
-                    let json = try JSONSerialization.jsonObject(with: data!, options: .mutableContainers)
-                    
-                    let responseJson = json as! [String: AnyObject]
-                    let tripsArray = responseJson["trips"]
-                    
-                    var trips = [Trip]()
-                    
-                    for dictionary in tripsArray as! [[String: AnyObject]] {
-                        let trip = Trip()
-                        trip.title = dictionary["name"] as! String?
-                        trip.numberOfLikes = dictionary["trip_likes_count"] as! NSNumber?
-                        trip.thumbnailImageURL = dictionary["thumbnail_image_url"] as! String?
-                        trip.isLikedByCurrentUser = (dictionary["is_liked_by_current_user"] as! Bool?)!
-                        
-                        
-                        // User Data
-                        // ---------------------
-                        let userDict = dictionary["user"] as! [String: AnyObject]
-                        let user = User()
-                        user.name = userDict["name"] as! String?
-                        user.isFollowedByCurrentUser = userDict["is_followed_by_current_user"] as! Bool?
-                        
-                        let profilePic = userDict["profile_pic"] as! [String: AnyObject]
-                        user.profileImageURL = profilePic["url"] as! String?
-                        // --------User Data----------
-                        
-                        trip.user = user
-                        
-                        trips.append(trip)
-                    }
-                    
-                    DispatchQueue.main.async {
-                         completion(trips)
-                    }
-                    
-                    return
-                }
-                catch let error as NSError {
-                    print(error)
-                }
-            }
-        }
-        task.resume()
+        fetchFeedForUrlString(urlString: "\(baseUrl)/trips.json", completion: completion)
     }
     
     func fetchTrendingTripsFeed(completion: @escaping ([Trip]) -> () ) {
-        let url = NSURL(string: "\(TripService.sharedData.API_URL)/trending/trips")
+        fetchFeedForUrlString(urlString: "\(baseUrl)/trending/trips", completion: completion)
+    }
+    
+    func fetchFeedForUrlString(urlString: String, completion: @escaping ([Trip]) -> () ) {
+        let url = NSURL(string: urlString)
         
         let configuration = URLSessionConfiguration.default
         
@@ -101,10 +48,10 @@ class TripService: NSObject {
                     
                     for dictionary in tripsArray as! [[String: AnyObject]] {
                         let trip = Trip()
-                        trip.title = dictionary["name"] as! String?
-                        trip.numberOfLikes = dictionary["trip_likes_count"] as! NSNumber?
-                        trip.thumbnailImageURL = dictionary["thumbnail_image_url"] as! String?
-                        trip.isLikedByCurrentUser = (dictionary["is_liked_by_current_user"] as! Bool?)!
+                        trip.name = dictionary["name"] as! String?
+                        trip.trip_likes_count = dictionary["trip_likes_count"] as! NSNumber?
+                        trip.thumbnail_image_url = dictionary["thumbnail_image_url"] as! String?
+                        trip.is_liked_by_current_user = (dictionary["is_liked_by_current_user"] as! Bool?)!
                         
                         
                         // User Data
@@ -112,10 +59,14 @@ class TripService: NSObject {
                         let userDict = dictionary["user"] as! [String: AnyObject]
                         let user = User()
                         user.name = userDict["name"] as! String?
-                        user.isFollowedByCurrentUser = userDict["is_followed_by_current_user"] as! Bool?
+                        user.is_followed_by_current_user = userDict["is_followed_by_current_user"] as! Bool?
                         
-                        let profilePic = userDict["profile_pic"] as! [String: AnyObject]
-                        user.profileImageURL = profilePic["url"] as! String?
+                        let profilePicDict = userDict["profile_pic"] as! [String: AnyObject]
+                        let profile_pic = ProfilePic()
+                        profile_pic.public_id = profilePicDict["public_id"] as! String?
+                        profile_pic.url = profilePicDict["url"] as! String?
+                        
+                        user.profile_pic = profile_pic
                         // --------User Data----------
                         
                         trip.user = user
--- TravelApp/User.swift
+++ TravelApp/User.swift
@@ -9,7 +9,24 @@
 import UIKit
 
 class User: NSObject {
+    var id: NSNumber?
     var name: String?
-    var profileImageURL: String?
-    var isFollowedByCurrentUser: Bool?
+    var email: String?
+    var instagram_access_token: String?
+    var instagram_profile_picture: String?
+    var instagram_user_name: String?
+    var profile_pic: ProfilePic?
+    var cover_photo: CoverPic?
+    var total_followers: NSNumber?
+    var total_following: NSNumber?
+    var total_trips: NSNumber?
+    var is_followed_by_current_user: Bool?
+}
+
+class ProfilePic: NSObject {
+    var url: String?
+    var public_id: String?
+}
+
+class CoverPic: ProfilePic {
 }
