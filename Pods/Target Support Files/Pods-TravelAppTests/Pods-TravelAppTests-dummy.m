1703b52d2e1a6327d0e629545908b2e24b89503b
nitpa7 <nitpa7@gmail.com>
2017-02-01 14:37:56 +0530

    Installing Cocoa

---
+++ Pods/Target Support Files/Pods-TravelAppTests/Pods-TravelAppTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TravelAppTests : NSObject
+@end
+@implementation PodsDummy_Pods_TravelAppTests
+@end
---
+++ Pods/Target Support Files/Pods-TravelAppUITests/Pods-TravelAppUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TravelAppUITests : NSObject
+@end
+@implementation PodsDummy_Pods_TravelAppUITests
+@end
