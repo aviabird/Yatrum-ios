a5107ef85b2bcb9d124bc28afb587c20fbbcedf6
Pankaj Rawat <pankajrawat19sept@gmail.com>
2017-02-02 12:33:08 +0530

    added effects

---
+++ TravelApp/Effects/TripEffect.swift
@@ -0,0 +1,19 @@
+//
+//  TripEffect.swift
+//  TravelApp
+//
+//  Created by Pankaj Rawat on 02/02/17.
+//  Copyright © 2017 Pankaj Rawat. All rights reserved.
+//
+
+import ReSwift
+
+func FetchTripsFeed(state: AppState, store: Store<AppState>) -> Action? {
+    TripService.sharedInstance.fetchTripsFeed { (trips: [Trip]) in
+        DispatchQueue.main.async {
+            store.dispatch(SetTrips(trips: trips))
+        }
+    }
+    
+    return nil
+}
