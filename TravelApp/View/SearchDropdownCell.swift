ef07aeb546d7d4fde389bfae9bfec1580e3a7791
nitpa7 <nitpa7@gmail.com>
2017-02-10 16:56:59 +0530

    Added Search Dropdown

---
+++ TravelApp/View/SearchDropdownCell.swift
@@ -0,0 +1,38 @@
+//
+//  SearchDropdownCell.swift
+//  TravelApp
+//
+//  Created by Nitesh on 10/02/17.
+//  Copyright © 2017 Pankaj Rawat. All rights reserved.
+//
+
+import UIKit
+
+class SearchDropdownCell: BaseCell {
+    
+    var searchedLabel : UILabel = {
+        let st = UILabel()
+        st.translatesAutoresizingMaskIntoConstraints = false
+        st.backgroundColor = UIColor.white
+        return st
+    }()
+    
+    
+    override func setupViews() {
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(selectSearch))
+        searchedLabel.isUserInteractionEnabled = true
+        searchedLabel.addGestureRecognizer(tapGesture)
+        
+        addSubview(searchedLabel)
+        searchedLabel.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        searchedLabel.widthAnchor.constraint(equalTo: self.widthAnchor).isActive = true
+        searchedLabel.heightAnchor.constraint(equalToConstant: 50).isActive = true
+    }
+    
+    func selectSearch() {
+        
+        print(searchedLabel.text!)
+    }
+    
+}
